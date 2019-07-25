a80ce5fbfcdf8871dbcb97f32eb93dfa5e1ec644
DanielJAch <danieljach@gmail.com>
2019-07-25 10:52:09 -0700

    Add configs for test, staging, and prod

--- build/webpack.staging.config.js
+++ build/webpack.staging.config.js
@@ -2,17 +2,17 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
 const CompressionWebpackPlugin = require('compression-webpack-plugin');
-const baseWebpackConfig = require('./base.config');
+const baseWebpackConfig = require('./webpack.base.config');
 
 module.exports = merge(baseWebpackConfig('production'), {
-  devtool: config.common.productionSourceMap ? '#source-map' : false,
+  devtool: false,
 
   plugins: [
     new TerserWebpackPlugin(),
     new CompressionWebpackPlugin({
       filename: '[path].gz[query]',
       algorithm: 'gzip',
-      test: new RegExp(`\\.(${config.common.productionGzipExtensions.join('|')})$`),
+      test: new RegExp(/\.(['js', 'css'])$/),
       threshold: 10240,
       minRatio: 0.8
     })
--- build/webpack.test.config.js
+++ build/webpack.test.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
-const baseWebpackConfig = require('./base.config');
+const baseWebpackConfig = require('./webpack.base.config');
 
 module.exports = merge(baseWebpackConfig('development'), {
   devtool: '#inline-source-map', // inline
